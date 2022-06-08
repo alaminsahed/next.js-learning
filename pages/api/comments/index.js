e37502e9c23e04c2132d7041dcd3e9d9df9bd532
alaminsahed <alaminsahed97@gmail.com>
2022-06-08 11:50:01 +0600

    post api

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,5 +1,17 @@
 import { comments } from "../../../data/comment"
 
 export default function handler(req, res) {
-    res.status(200).json(comments)
+    if (req.method === 'GET') {
+        res.status(200).json(comments)
+
+    }
+    else if (req.method === 'POST') {
+        const data = req.body.comment;
+        const newComment = {
+            id: Date.now(),
+            text: data
+        }
+        comments.push(newComment);
+        res.status(201).json(comments);
+    }
 }
--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,14 +2,31 @@ import React from 'react';
 
 const Comments = () => {
     const [comments, setComments] = React.useState([]);
+    const [comment, setComment] = React.useState('');
 
     const fetchComments = async () => {
         const response = await fetch('http://localhost:3000/api/comments');
         const data = await response.json();
         setComments(data);
     }
+
+    const submitComments = async () => {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ comment })
+        });
+        const data = await response.json();
+        console.log(data);
+    }
+
+
     return (
         <div>
+            <input type="text" value={comment} onChange={(e) => setComment(e.target.value)} />
+            <button onClick={submitComments}>submit</button>
             <button onClick={fetchComments}>Comments</button>
             {
                 comments.map((item, index) => {
